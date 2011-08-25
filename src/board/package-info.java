015c244fd2bcbd8f4ef165d18b1640501e547d86
Olof-Joachim Frahm <Olof.Frahm@web.de>
2011-08-25 21:48:19 +0200

    Added javadoc package information.

---
+++ src/board/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains all Whiteboard classes, implementing a global shared database
+ * for semi-structured RDF data and a publish-subscribe model for event
+ * data from sensors and generic clients.
+ * <p>
+ * Accessing RDF data is done using the Jena and ARQ framework.
+ * @see com.hp.hpl.jena
+ * @see com.hp.hpl.jena.sparql
+ */
+package board;
---
+++ src/board/vocabulary/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains helper classes to represent RDF URIs with a simple Java API.
+ * @see com.hp.hpl.jena.vocabulary
+ */
+package board.vocabulary;
