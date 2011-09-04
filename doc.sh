9ccf3f8401215a4acc5c94382261b699ab8a6b44
Olof-Joachim Frahm <Olof.Frahm@web.de>
2011-09-04 18:50:07 +0200

    Added external links for javadoc.

--- doc.sh
+++ doc.sh
@@ -10,4 +10,8 @@ LOG=$(echo lib/slf4j/slf4j-$LOGGING*.jar)
 PACKAGES="board board.vocabulary comm hub mail reasoner sms"
 
 # exec gjdoc -validhtml -verbose -private -d javadoc -all -classpath bin:src:$LIBS:$LOG -s src
-exec javadoc -verbose -private -d javadoc -classpath bin:src:$LIBS:$LOG -sourcepath src $PACKAGES
+exec javadoc \
+  -link http://download.oracle.com/javase/7/docs/api/ \
+  -link http://jena.sourceforge.net/javadoc/ \
+  -link http://jena.sourceforge.net/ARQ/javadoc/ \
+  -verbose -private -d javadoc -classpath bin:src:$LIBS:$LOG -sourcepath src $PACKAGES
